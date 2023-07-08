57d474ff9aed0b120e6567868fde9b170e91e893
Arunava Dutta <121813676+ArunavaCoderEm@users.noreply.github.com>
2023-07-08 23:45:00 +0530

    Update dsahackerrankpy.py

--- dsahackerrankpy.py
+++ dsahackerrankpy.py
@@ -577,3 +577,33 @@ def kangaroo(x1, v1, x2, v2):
         
 y = kangaroo(0,2,5,3)
 print(y)
+
+
+# # # # HACKERRANK BILL DIVISION
+
+import math
+import os
+import random
+import re
+import sys
+
+def bonAppetit(bill, k, b):
+    bill.remove(bill[k])
+    sumla = int(sum(bill)/2)
+    if (sumla == b):
+        print("Bon Appetit")   
+    else:
+        print(abs(b-sumla)) 
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
