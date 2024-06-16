afd65552c80e186fe616a9b8b8a34d720525104a
ArunavaCoderEm <acecankill@gmail.com>
2024-06-16 19:31:34 +0530

    push

--- dsahackerrankpy.py
+++ dsahackerrankpy.py
@@ -901,3 +901,26 @@ class Solution:
         return ans
 sol = Solution()
 print(sol.getStrongest([1,2,3,4,5],2))
+
+from typing import List
+
+class Solution:
+    def countCompleteDayPairs(self, hours: List[int]) -> int:
+        count = 0
+        for i in range(len(hours) - 1):
+            for j in range(i+1, len(hours)):
+                if((hours[i] + hours[j]) % 24 == 0): count += 1
+        return count
+
+class Solution:
+    # Hint 2 .. has given the answer only LOL
+    def countCompleteDayPairs(self, hours: List[int]) -> int:
+        count = 0
+        res = [0]*24
+        for j in hours:
+            count += res[(24-(j%24)) % 24]
+            res[j % 24] += 1
+        return count
+
+sol = Solution()
+print(sol.countCompleteDayPairs([12,12,30,24,24]))
