5942c9284c2a1f6f4729ee5e7f0f1c8699c6b9fb
ArunavaCoderEm <acecankill@gmail.com>
2024-07-11 17:26:57 +0530

    push to GH

--- dsahackerrankpy.py
+++ dsahackerrankpy.py
@@ -1118,3 +1118,31 @@ class Solution:
 sol = Solution()
 driverip = "(ed(et(oc))el)"
 print(sol.reverseParentheses(driverip))
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        
+        stk = [num[0]]
+        i = 1        
+        
+        
+        while (i < len(num)):
+            while (k > 0):
+                if (len(stk) and stk[-1] > num[i]):
+                    k -= 1
+                    stk.pop()
+                else: break
+            stk.append(num[i])
+            i += 1
+            
+        while (k > 0):
+            stk.pop()
+            k -= 1
+
+        while (len(stk) and stk[0] == "0"): stk.pop(0)
+            
+        retres = ""
+
+        retres = "".join(stk)
+        
+        return "0" if (retres == "") else retres
