021d662b19d3b4337ee29b007f3b6adf59f1deff
ArunavaCoderEm <acecankill@gmail.com>
2024-07-12 14:18:56 +0530

    push GH

--- dsahackerrankpy.py
+++ dsahackerrankpy.py
@@ -1238,3 +1238,39 @@ class Solution:
 sol = Solution()
 nums = [4,14,2]
 print(sol.totalHammingDistance(nums))
+
+
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        
+        i = 0
+        j = len(s) - 1
+
+        s = list(s)
+        
+        vow = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+        while (i < j):
+            if (s[i] in vow and s[j] in vow):
+                s[i], s[j] = s[j], s[i]
+                i += 1
+                j -= 1
+                print(s, i, j)
+            elif (s[i] in vow and s[j] not in vow):
+                j -= 1
+                print(s, i, j)
+            elif (s[j] in vow and s[i] not in vow):
+                i += 1
+                print(s, i, j)
+            
+            elif (s[i] not in vow and s[j] not in vow):
+                i += 1
+                j -= 1
+        
+        res = "".join(s)
+        
+        return res
+    
+sol = Solution()
+s = "aA"
+print(sol.reverseVowels(s))
